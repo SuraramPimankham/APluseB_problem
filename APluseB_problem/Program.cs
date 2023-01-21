2ae39d306cec540927134c160d6623e744cebdee
Suraram Pimankham <76487588+SuraramPimankham@users.noreply.github.com>
2023-01-21 23:23:29 +0700

    Initial commit

---
+++ APluseB_problem/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace APluseB_problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = Int32.Parse(Console.ReadLine());
+            int B = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(A+B);
+        }
+    }
+}
